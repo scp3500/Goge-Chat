809f5c47973ebaa0368f6f332a6691d320775234
scp3500 <3379536483@qq.com>
2026-02-02 09:11:29 +0800

    new

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,21 +1,2 @@
 /// <reference types="vite/client" />
 
-declare module '*.svg' {
-    const content: string;
-    export default content;
-}
-
-declare module '*.svg?url' {
-    const content: string;
-    export default content;
-}
-
-declare module '*.svg?raw' {
-    const content: string;
-    export default content;
-}
-
-declare module '*.md?raw' {
-    const content: string;
-    export default content;
-}
